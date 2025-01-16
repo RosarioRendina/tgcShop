4fc42c495294a96998c0964ac5814992a931e11e
Sandro-Pavone <183720228+Sandro-Pavone@users.noreply.github.com>
2025-01-16 18:45:35 +0100

    update panel

--- Front/assets/scripts/panel.js
+++ Front/assets/scripts/panel.js
@@ -66,9 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
             modal.hide();
             console.log(product);
             location.reload();
-            
         })
-
         .catch(function(error) {
             console.error("Errore fetch nell'inserimento del prodotto:", error);
         });
@@ -142,27 +140,61 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById('edit-rimanenza').value = product.rimanenza;
         document.getElementById('edit-abilitato').checked = product.abilitato;
         document.getElementById('edit-visibile').checked = product.visibile;
-        document.getElementById('edit-immagine').value = product.imgUrl;
         document.getElementById('edit-inizio_prevendita').value = product.inizio_prevendita;
         document.getElementById('edit-data_uscita').value = product.data_uscita;
         document.getElementById('edit-sconto_prevendita').value = product.sconto_prevendita;
+
+        // Gestione del campo immagine
+        const editImmagineInput = document.getElementById('edit-immagine');
+        const editImmaginePreview = document.getElementById('edit-immagine-preview');
+
+        // // Mostriamo l'anteprima dell'immagine esistente, se presente
+        // if (product.imgUrl && product.imgUrl !== "non-disponibile.jpg") {
+        //     editImmaginePreview.src = product.imgUrl;
+        //     editImmaginePreview.style.display = "block"; // Mostra l'anteprima
+        // } else {
+        //     // Se l'immagine è di default o non esiste, non mostriamo l'anteprima
+        //     editImmaginePreview.style.display = "none";
+        // }
+
+        console.log(product.imgUrl);
+        editImmaginePreview.src = product.imgUrl;
+
+        editImmagineInput.addEventListener("change", (e) => {
+            const file = e.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = e => {
+                    editImmaginePreview.src = e.target.result
+                }
+                reader.readAsDataURL(file);
+            } else {
+                editImmaginePreview.src = "";
+            }
+        })
+        
+        
+
+        // Impostiamo il tipo del campo immagine a "file" e lo lasciamo vuoto
+        editImmagineInput.type = 'file';
+        editImmagineInput.value = ''; // Rimuoviamo il valore, poiché non possiamo pre-compilare un campo file con un valore
+
         document.getElementById('edit-product-form').dataset.productId = product.prodottoId;
     }
 
     // Funzione per aprire la modale di modifica
     function openEditProductModal(product) {
-        
         fillFormWithProductData(product);
 
         const editProductModal = document.getElementById('edit-product-modal');
         const modal = new bootstrap.Modal(editProductModal);
         modal.show();
 
-        // Imposta il tasto di reset con i dati originali del prodotto
+        // Impostiamo il tasto di reset con i dati originali del prodotto
         const resetEditProductButton = document.getElementById('reset-edit-product-btn');
         resetEditProductButton.addEventListener('click', () => {
-        resetForm(product); // Passa il prodotto corrente per ripristinare i dati originali
-    });
+            resetForm(product); // Passa il prodotto corrente per ripristinare i dati originali
+        });
     }
 
     // Funzione per eliminare un prodotto
@@ -190,52 +222,78 @@ document.addEventListener("DOMContentLoaded", () => {
     // Gestisce l'invio del modulo per la modifica di un prodotto
     const editProductForm = document.getElementById('edit-product-form');
     editProductForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+        event.preventDefault();
 
-    // Estrai l'ID del prodotto dalla modale
-    const prodottoId = editProductForm.dataset.productId;
-    const editedProduct = {
-        prodottoId: prodottoId, // Aggiungi l'ID del prodotto
-        nome: document.getElementById('edit-nome').value,
-        descrizione: document.getElementById('edit-descrizione').value,
-        categoria: document.getElementById('edit-categoria').value,
-        prezzo: parseFloat(document.getElementById('edit-prezzo').value),
-        rimanenza: parseInt(document.getElementById('edit-rimanenza').value),
-        immagine: document.getElementById('edit-immagine').value,
-        abilitato: document.getElementById('edit-abilitato').checked,
-        visibile: document.getElementById('edit-visibile').checked,
-        inizio_prevendita: document.getElementById('edit-inizio_prevendita').value,
-        data_uscita: document.getElementById('edit-data_uscita').value,
-        sconto_prevendita: parseFloat(document.getElementById('edit-sconto_prevendita').value)
-    };
+        // Estrai l'ID del prodotto dalla modale
+        const prodottoId = editProductForm.dataset.productId;
+        const editedProduct = {
+            prodottoId: prodottoId, // Aggiungi l'ID del prodotto
+            nome: document.getElementById('edit-nome').value,
+            descrizione: document.getElementById('edit-descrizione').value,
+            categoria: document.getElementById('edit-categoria').value,
+            prezzo: parseFloat(document.getElementById('edit-prezzo').value),
+            rimanenza: parseInt(document.getElementById('edit-rimanenza').value),
+            // immagine: document.getElementById('edit-immagine').type === 'file' ? document.getElementById('edit-immagine').files[0] : null,
+            abilitato: document.getElementById('edit-abilitato').checked,
+            visibile: document.getElementById('edit-visibile').checked,
+            inizio_prevendita: document.getElementById('edit-inizio_prevendita').value,
+            data_uscita: document.getElementById('edit-data_uscita').value,
+            sconto_prevendita: parseFloat(document.getElementById('edit-sconto_prevendita').value)
+        };
 
-    // Effettua la richiesta PUT al backend per aggiornare il prodotto
-    fetch(`http://localhost:8080/api/prodotto`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(editedProduct)
-    })
-    .then(function(response) {
-        if (response.ok) {
-            alert("Prodotto modificato con successo!");
-            fetchProducts(); // Ricarica la lista dei prodotti
-            const modal = bootstrap.Modal.getInstance(document.getElementById('edit-product-modal'));
-            modal.hide(); // Chiudi la modale
+        const editImmagine = document.getElementById('edit-immagine').type === 'file' ? document.getElementById('edit-immagine').files[0] : null;
+
+        console.log(editImmagine);
+
+
+        const formData = new FormData();
+        formData.append("prodotto", JSON.stringify(editedProduct));
+        if (editImmagine) {
+            formData.append("image", editImmagine); // Aggiungi immagine solo se presente
         } else {
-            throw new Error("Errore nella modifica del prodotto");
+            console.log("Nessuna immagine selezionata");
         }
-    })
-    .catch(function(error) {
-        console.error("Errore durante la modifica del prodotto:", error);
+        
+        console.log("FormData:", formData); // Controlla i dati nel formData
+        // formData.append("image", editImmagine);
+
+        fetch(`http://localhost:8080/api/prodotto/alt`, {
+            method: 'PUT',
+            body: formData
+        })
+        .then(function(response) {
+            if (response.ok) {
+                alert("Prodotto modificato con successo!");
+                fetchProducts(); // Ricarica la lista dei prodotti
+                const modal = bootstrap.Modal.getInstance(document.getElementById('edit-product-modal'));
+                modal.hide(); // Chiudi la modale
+            } else {
+                throw new Error("Errore nella modifica del prodotto");
+            }
+        })
+        .catch(function(error) {
+            console.error("Errore durante la modifica del prodotto:", error);
+        });
+    });
+
+    function resetForm(product) {
+        fillFormWithProductData(product); // Ripristina i dati originali nel modulo
+    }
+
+    // Gestisce l'anteprima dell'immagine
+    document.getElementById('edit-immagine').addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        const preview = document.getElementById('edit-immagine-preview');
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                preview.src = e.target.result;
+                preview.style.display = "block"; // Mostra l'anteprima dell'immagine
+            };
+
+            reader.readAsDataURL(file);
+        }
     });
 });
-
-function resetForm(product) {
-    fillFormWithProductData(product); // Ripristina i dati originali nel modulo
-}
-
-
- 
-}); 
