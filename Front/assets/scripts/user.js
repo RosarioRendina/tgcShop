9dc61e3ef44123570b0d9af6b401b6d79069a2b5
Eugenio Caiazzo <152604171+EugenioCaiazz0@users.noreply.github.com>
2025-01-16 12:56:14 +0100

    User logica completata feat.Sandro Pavone

--- Front/assets/scripts/user.js
+++ Front/assets/scripts/user.js
@@ -60,21 +60,33 @@ function createOrdersRow(ordini) {
     const container = document.getElementById("ordini-container");
     // Assicurati che il contenitore sia svuotato prima di aggiungere nuove righe
     container.innerHTML = "";
-    console.log(("Rendering table in ", container, " with ", ordini.length, " rows"));
+    console.log(("Rendering table with " + ordini.length + " rows in element: " + container.id));
 
     ordini.forEach(ordine => {
         //creazione row per ogni ordine
         const row = document.createElement('tr');
         row.setAttribute('ordine-ID', ordine.ordineId);
 
+        if(ordine.statoOrdine===IN_LAVORAZIONE){
+            row.innerHTML = `
+            <td>${ordine.dataConsegna || "Non disponibile"}</td>
+            <td>${ordine.dataOrdine}</td>
+            <td>${ordine.indirizzoSpedizione}</td>
+            <td>${ordine.statoOrdine}</td>
+             <td>
+                <button class="btn btn-primary details-order-btn">Dettagli</button>
+                <button class="btn btn-warning cancel-order-btn">Annulla</button>
+            </td>
+        `;
+        } else 
         row.innerHTML = `
             <td>${ordine.dataConsegna || "Non disponibile"}</td>
             <td>${ordine.dataOrdine}</td>
             <td>${ordine.indirizzoSpedizione}</td>
             <td>${ordine.statoOrdine}</td>
-            <td>
-                <button class="btn btn-warning cancel-order-btn">Annulla</button>
-                <button class="btn btn-danger delete-order-btn">Elimina</button>
+             <td>
+                <button class="btn btn-primary details-order-btn">Dettagli</button>
+                <button class="btn btn-warning cancel-order-btn d-none">Annulla</button>
             </td>
         `;
 
@@ -83,16 +95,45 @@ function createOrdersRow(ordini) {
             console.log(`Annulla ordine con ID ${ordine.ordineId}`);
             // Implementa la logica per annullare l'ordine
         });
-
-        row.querySelector('.delete-order-btn').addEventListener('click', () => {
-            console.log(`Elimina ordine con ID ${ordine.ordineId}`);
-            // Implementa la logica per eliminare l'ordine
+        row.querySelector('.details-order-btn').addEventListener('click', () => {
+            openDetailsModal(ordine);
         });
 
         // Aggiungi la riga al contenitore
         container.appendChild(row);
     });
 }
+
+
+// ! MODALE DETTAGLI DA VERIFICARE
+    function fillDetails(ordine) {
+  /*
+    document.getElementById('edit-nome').value = product.nome;
+    document.getElementById('edit-descrizione').value = product.descrizione || "N/A";
+    document.getElementById('edit-categoria').value = product.categoria || "N/A";
+    document.getElementById('edit-prezzo').value = product.prezzo;
+    document.getElementById('edit-rimanenza').value = product.rimanenza;
+    document.getElementById('edit-abilitato').checked = product.abilitato;
+    document.getElementById('edit-visibile').checked = product.visibile;
+    document.getElementById('edit-immagine').value = product.imgUrl;
+    document.getElementById('edit-inizio_prevendita').value = product.inizio_prevendita;
+    document.getElementById('edit-data_uscita').value = product.data_uscita;
+    document.getElementById('edit-sconto_prevendita').value = product.sconto_prevendita;
+    document.getElementById('edit-product-form').dataset.productId = product.prodottoId;
+    */
+}
+
+    // Funzione per aprire la modale di modifica
+    function openDetailsModal(ordine) {
+        
+        fillDetails(ordine);
+
+        const detailsModal = document.getElementById('order-details');
+        const modal = new bootstrap.Modal(detailsModal);
+        modal.show();
+    }
+    
+// ! MODALE DETTAGLI DA VERIFICARE
 
  // funzione per creare la tabella degli ordini
  function filterAndDisplayOrders(ordini){
