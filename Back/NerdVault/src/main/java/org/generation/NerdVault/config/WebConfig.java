9aee9e005b3cc7ff452977bd0de1a64613089d45
Rosario Rendina <161160724+RosarioRendina@users.noreply.github.com>
2025-01-17 15:37:08 +0100

    ariecco la cors - errore 404

---
+++ Back/NerdVault/src/main/java/org/generation/NerdVault/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.generation.NerdVault.config;
+
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+		 System.out.println("Configurazione CORS applicata!");
+        
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+		}
+}
